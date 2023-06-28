b73cec9cb08081a665089886c566f3b9d6efcd7b
WillR <willrich@live.co.uk>
2023-06-28 12:43:25 +0300

    Not finished

--- app/src/main/java/com/example/william_rich_lab7/MainActivity.java
+++ app/src/main/java/com/example/william_rich_lab7/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.navigation.NavigationView;
 
@@ -77,6 +78,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             System.exit(0);
 
         }
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
 
         return true;
     }
