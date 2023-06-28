5c0b4e51b97e43d010ef062cc90cdc723f00da25
WillR <willrich@live.co.uk>
2023-06-28 12:39:53 +0300

    Final

--- app/src/main/java/com/example/william_rich_lab7/MainActivity.java
+++ app/src/main/java/com/example/william_rich_lab7/MainActivity.java
@@ -11,7 +11,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.navigation.NavigationView;
 
